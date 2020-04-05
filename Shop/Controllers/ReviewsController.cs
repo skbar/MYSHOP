e9941dd9aba6253891dfc0cacbf5df5ab4cdd19e
djsb91 <sbaranczuk@gmail.com>
2020-04-05 13:49:25 +0200

    Reviews

---
+++ Shop/Controllers/ReviewsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Shop.Controllers
+{
+    public class ReviewsController : BaseController
+    {
+        // GET: Reviews
+        public ActionResult Index([Bind(Prefix ="Id")]int productId)
+        {
+            var product = _db.Products.Find(productId);
+            if (product != null)
+            {
+                return View(product);
+            }
+                return HttpNotFound();
+        }
+    }
+}
---
+++ Shop/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Shop.Models
+{
+    public class Product : IEntity
+    {
+        
+        public string Name { get; set; }
+        public string Descroption { get; set; }
+        public decimal Price { get; set; }
+        //public virtual Category Category { get; set; }
+        public virtual ICollection<Review> Reviews { get; set; }
+    }
+}
